6b84f0a20f5ca3a5ab436175c7c617f964e05eb4
milojeBtc <nikolic.miloje0507@gmail.com>
2024-09-18 16:07:36 +0100

    chore: remove swagger Ui

--- multisig-wallet/index.ts
+++ multisig-wallet/index.ts
@@ -8,22 +8,17 @@ import cron from "node-cron";
 import { PORT, connectMongoDB } from "./config";
 import http from "http";
 import {
-  UserRouter,
   SendBtcRoute,
   multiSigWalletRoute,
   testRoute,
-  airdropVaultRoute,
 } from "./routes";
 import requestRouter from "./routes/request";
 import runeRoute from "./routes/rune";
 import { checkTxStatus } from "./controller/rune.controller";
-import marketplaceRoute from "./routes/marketplace";
 
 // Swagger UI implementation
 import swaggerUi from "swagger-ui-express";
 import YAML from "yamljs";
-import syndicateVaultRoute from "./routes/syndicateVault";
-import stakingVaultRouter from "./routes/stakingVault";
 
 // Load environment variables from .env file
 dotenv.config();
@@ -52,16 +47,11 @@ const server = http.createServer(app);
 let swaggerDocument: any = YAML.load("swagger.yaml");
 
 // Define routes for different API endpoints
-app.use("/api/users", UserRouter);
 app.use("/api/sendBTC", SendBtcRoute);
 app.use("/api/multisig", multiSigWalletRoute);
-app.use("/api/airdrop", airdropVaultRoute);
 app.use("/api/request", requestRouter);
 app.use("/api/rune", runeRoute);
 app.use("/api/test", testRoute);
-app.use("/api/marketplace", marketplaceRoute);
-app.use("/api/syndicate", syndicateVaultRoute);
-app.use("/api/staking", stakingVaultRouter);
 
 // Swagger endpoint Settings
 app.use(
