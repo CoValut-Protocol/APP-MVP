0a439c5c5b03bbfc90ab078ce0bb7823adf8ffb7
milojeBtc <nikolic.miloje0507@gmail.com>
2024-09-18 16:28:46 +0100

    chore: update route index

--- multisig-wallet/routes/index.ts
+++ multisig-wallet/routes/index.ts
@@ -1,8 +1,6 @@
-import UserRouter from "./UserRoute";
 import SendBtcRoute from "./sendBtcRoute";
 import multiSigWalletRoute from "./multisig";
-import airdropVaultRoute from "./airdropVault";
 import runeRoute from "./rune";
 import testRoute from "./test";
 
-export { UserRouter, SendBtcRoute, multiSigWalletRoute, runeRoute, airdropVaultRoute, testRoute }
+export { SendBtcRoute, multiSigWalletRoute, runeRoute, testRoute }
